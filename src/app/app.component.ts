a35508426e65e67c03421c134f0b9107a887e554
Yiwei Wu <yiweiwu@Yiweis-MacBook-Pro.local>
2023-08-15 19:17:31 +1000

    finish practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+// import {COURSES} from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +14,18 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  courses$: Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
+  constructor( private courseService: CoursesService) {
 
   }
 
   ngOnInit() {
+    this.courses$ = this.courseService.loadCourses() ;
   }
 
-
-
+  save(course:Course){
+    console.log('Check here', course);
+    this.courseService.saveCourse(course).subscribe();
+  }
 }
